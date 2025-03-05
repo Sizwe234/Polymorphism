3faf8c28426d1ae7615689116baca3da5f3319df
lab_services_student <lab_services_student@lab7L95SR>
2025-03-05 15:24:30 +0200

    Add project files.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal makes a sound");
+        }
+    }
+
+
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Dog Barks");
+        }
+
+    }
+    class Program
+    {
+
+        static void Main()
+        {
+            Animal myAnimal = new Dog();  // Parent class reference holding a child object
+            myAnimal.MakeSound();
+        }
+    }
+}
